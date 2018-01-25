e0d56da3cbbd3d00edc426084cfcd2ed979e9352
LeonidIvankin <lim14@yandex.ru>
2018-01-25 21:53:06 +0300

    Добавил базу. Создал таблицу

---
+++ DB/src/Main.java
@@ -0,0 +1,65 @@
+import java.sql.*;
+
+public class Main {
+
+	private static Connection connection;
+	private static Statement statement;
+	private static PreparedStatement preparedStatement;
+
+	/*CREATE TABLE users (
+    id   INTEGER PRIMARY KEY AUTOINCREMENT,
+    name STRING,
+    pass STRING
+);*/
+
+
+
+	public static void main(String[] args) {
+		try {
+			connect();
+			//addData();
+			//updateData();
+			//deleteData();
+			getData();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} finally {
+			disconnect();
+		}
+
+	}
+
+	private static void addData() throws SQLException {
+		statement.executeUpdate("INSERT INTO users (name, pass) VALUES ('Alexandra', '1234')");
+	}
+
+	private static void updateData() throws SQLException {
+		statement.executeUpdate("UPDATE users SET pass = 1111 WHERE name = 'Leonid'");
+	}
+
+	private static void deleteData() throws SQLException {
+		statement.executeUpdate("DELETE FROM users WHERE name = 'Leonid';");
+	}
+
+	private static void getData() throws SQLException {
+		ResultSet resultSet = statement.executeQuery("SELECT id, name FROM users WHERE name = 'Alexandra';");
+		while(resultSet.next()) System.out.println(resultSet.getInt("id") + " " + resultSet.getString("name"));
+	}
+
+	public static void connect() throws SQLException, ClassNotFoundException{
+		Class.forName("org.sqlite.JDBC");
+		connection = DriverManager.getConnection("jdbc:sqlite:DB/dropbox.db");
+		statement = connection.createStatement();
+	}
+	public static void disconnect(){
+		try {
+			statement.close();
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
